2447fc51e8ea53cd39fea6eef5e0e6229beb78ec
esteban <14810250+esteban-url@users.noreply.github.com>
2022-09-20 09:15:59 -0600

    Reorder methods

--- src/calculateGPA/calculateGPA.ts
+++ src/calculateGPA/calculateGPA.ts
@@ -1,12 +1,17 @@
 /**
- * Round a number to a certain number of decimal places
- * @param {number} value to be rounded
- * @param {number} precision number of decimal places
- * @returns {number} rounded value
+ * Given a set of letter grades, output the GPA (grade point average) of those grades.
+ * @param {string[]} grades
+ * @returns {number} the Grade Point Average (GPA) with 1 decimal place
+ * @example calculateGPA(['F', 'F', 'F']) // 0
+ * @example calculateGPA(['A', 'A-', 'B+', 'B', 'B-']) // 3.3
+ * @example calculateGPA(['A', 'B+', 'C-', 'A']) // 3.3
  */
-const round = (value: number, precision: number = 0): number => {
-  const multiplier = Math.pow(10, precision)
-  return Math.round(value * multiplier) / multiplier
+export const calculateGPA = (grades: string[]): number => {
+  const total = grades.reduce((subTotal, grade) => {
+    return subTotal + gradeKey[grade.toLocaleUpperCase()]
+  }, 0)
+
+  return round(total / grades.length, 1)
 }
 
 const gradeKey = {
@@ -25,17 +30,13 @@ const gradeKey = {
 }
 
 /**
- * Given a set of letter grades, output the GPA (grade point average) of those grades.
- * @param {string[]} grades
- * @returns {number} the Grade Point Average (GPA) with 1 decimal place
- * @example calculateGPA(['F', 'F', 'F']) // 0
- * @example calculateGPA(['A', 'A-', 'B+', 'B', 'B-']) // 3.3
- * @example calculateGPA(['A', 'B+', 'C-', 'A']) // 3.3
+ * Round a number to a certain number of decimal places
+ * @param {number} value to be rounded
+ * @param {number} precision number of decimal places
+ * @returns {number} rounded value
  */
-export const calculateGPA = (grades: string[]): number => {
-  const total = grades.reduce((subTotal, grade) => {
-    return subTotal + gradeKey[grade.toLocaleUpperCase()]
-  }, 0)
-
-  return round(total / grades.length, 1)
+const round = (value: number, precision: number = 0): number => {
+  const multiplier = Math.pow(10, precision)
+  return Math.round(value * multiplier) / multiplier
 }
+
