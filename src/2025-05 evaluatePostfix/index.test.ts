50a573590c49a7e61be3e05e20506c870cc20d2c
esteban-url <14810250+esteban-url@users.noreply.github.com>
2025-02-18 00:12:40 -0600

    test: fix expected output for evaluatePostfix function in test cases

--- src/2025-05 evaluatePostfix/index.test.ts
+++ src/2025-05 evaluatePostfix/index.test.ts
@@ -10,6 +10,6 @@ describe('2025-03 findAnagrams', () => {
     expect(f('56+7*')).toEqual(77)
   })
   test('"56+72*" should equal 154', () => {
-    expect(f('56+72**')).toEqual(154)
+    expect(f('56+72*')).toEqual(154)
   })
 })
